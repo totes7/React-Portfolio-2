8b6e4cb978fa291eaf83a31a3a5fe6fcd50772b5
Antonio Manno <antonio.manno.7@gmail.com>
2024-08-22 19:06:32 +0100

    finalise home section pattern for all screen sizes; first draw of home bio section

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import '../../App.css';
 import "./Home.css";
 
 function Home() {
 return (
   <div className="home">
 
-    <div className="bio">
+    <div className="bio br-2">
       <h1>Antonio Manno</h1>
       <h3>Junior Web Developer</h3>
+      <p>I'm a Full Stack Web Developer with a specialization in Front End development. Eager to leverage my skills and knowledge to contribute effectively to dynamic web development projects. Committed to delivering innovative, user-centric solutions while continuously enhancing my proficiency in cutting-edge technologies.</p>
     </div>
-    <div className="skills"></div>
-    <div className="portfolio"></div>
-    <div className="contact"></div>
+    <div className="skills br-2"></div>
+    <div className="portfolio br-2"></div>
+    <div className="contact br-2"></div>
 
   </div>
 );
