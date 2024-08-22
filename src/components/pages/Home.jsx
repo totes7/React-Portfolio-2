fe772ed46283274850ee28c8c566bd82a0d08cb7
Antonio Manno <antonio.manno.7@gmail.com>
2024-08-22 19:34:49 +0100

    complete skills section in home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import '../../App.css';
+import "../../App.css";
 import "./Home.css";
 
 function Home() {
-return (
-  <div className="home">
-
-    <div className="bio br-2">
-      <h1>Antonio Manno</h1>
-      <h3>Junior Web Developer</h3>
-      <p>I'm a Full Stack Web Developer with a specialization in Front End development. Eager to leverage my skills and knowledge to contribute effectively to dynamic web development projects. Committed to delivering innovative, user-centric solutions while continuously enhancing my proficiency in cutting-edge technologies.</p>
+  return (
+    <div className="home">
+      <div className="bio br-2">
+        <h1>Antonio Manno</h1>
+        <h3>Junior Web Developer</h3>
+        <p>
+          I'm a Full Stack Web Developer with a specialization in Front End
+          development. Eager to leverage my skills and knowledge to contribute
+          effectively to dynamic web development projects. Committed to
+          delivering innovative, user-centric solutions while continuously
+          enhancing my proficiency in cutting-edge technologies.
+        </p>
+      </div>
+      <div className="skills br-2">
+        <div id="title">
+          <h2>Main Skills</h2>
+        </div>
+        <i id="html" class="fa-brands fa-html5"></i>
+        <i id="css" class="fa-brands fa-css3-alt"></i>
+        <i id="react" class="fa-brands fa-react"></i>
+        <i id="python" class="fa-brands fa-python"></i>
+      </div>
+      <div className="portfolio br-2"></div>
+      <div className="contact br-2"></div>
     </div>
-    <div className="skills br-2"></div>
-    <div className="portfolio br-2"></div>
-    <div className="contact br-2"></div>
-
-  </div>
-);
+  );
 }
 
 export default Home;
